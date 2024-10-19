2a1fa1506fc0632c399f6f948524d027f0814987
matas234 <160629386+matas234@users.noreply.github.com>
2024-10-19 23:58:08 +0100

    final

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ def create_go_board(
     board_pixel_size = ((board_size - 1) * grid_size) + 2 * border_size
 
     if background == "color":
-        board_image = Image.new(
-            "RGB", (board_pixel_size, board_pixel_size), color=bg_color
-        )
+        board_image = Image.new("RGB", (board_pixel_size, board_pixel_size), color=bg_color)
 
     elif background == "texture":
         texture = Image.open("assets/wood_texture.jpg").resize((board_pixel_size, board_pixel_size))
@@ -23,9 +21,7 @@ def create_go_board(
         board_image.paste(texture, (0, 0))
 
     elif background == "transparent":
-        board_image = Image.new(
-            "RGBA", (board_pixel_size, board_pixel_size), (0, 0, 0, 0)
-        )
+        board_image = Image.new("RGBA", (board_pixel_size, board_pixel_size), (0, 0, 0, 0))
 
     else:
         raise ValueError("Unsupported background type")
@@ -33,33 +29,25 @@ def create_go_board(
     draw = ImageDraw.Draw(board_image)
 
     for i in range(board_size):
-        draw.line(
-            (
+        draw.line((
                 border_size + i * grid_size,
                 border_size,
                 border_size + i * grid_size,
-                board_pixel_size - border_size,
-            ),
-            fill=line_color,
-        )
+                board_pixel_size - border_size),
+            fill=line_color)
 
-        draw.line(
-            (
+        draw.line((
                 border_size,
                 border_size + i * grid_size,
                 board_pixel_size - border_size,
-                border_size + i * grid_size,
-            ),
-            fill=line_color,
-        )
+                border_size + i * grid_size),
+            fill=line_color)
 
     return board_image
 
 
 def generate_qr_matrix(data):
-    qr = qrcode.QRCode(
-        box_size=1, border=0, error_correction=qrcode.constants.ERROR_CORRECT_L
-    )  ## change this for error correcting (currently on the lowest)
+    qr = qrcode.QRCode(box_size=1, border=0, error_correction=qrcode.constants.ERROR_CORRECT_L)  ## change this for error correcting (currently on the lowest)
     qr.add_data(data)
     qr.make(fit=True)
 
@@ -80,13 +68,9 @@ def create_go_board_with_qr(
 
     stone_size = int(grid_size * stone_scaling)
 
-    black_stone = (
-        Image.open(f"assets/b16.png").resize((stone_size, stone_size)).convert("RGBA")
-    )
-    white_stone = (
-        Image.open(f"assets/w16.png").resize((stone_size, stone_size)).convert("RGBA")
-    )
-
+    black_stone = Image.open(f"assets/b16.png").resize((stone_size, stone_size)).convert("RGBA")
+    white_stone = Image.open(f"assets/w16.png").resize((stone_size, stone_size)).convert("RGBA")
+    
     qr_matrix = generate_qr_matrix(data)
     qr_size = len(qr_matrix)
 
@@ -103,7 +87,6 @@ def create_go_board_with_qr(
             cx = border_size + x * grid_size + (grid_size - stone_size) // 2 - grid_size // 2
             cy = border_size + y * grid_size + (grid_size - stone_size) // 2 - grid_size // 2
 
-
             board_image.paste(black_stone if qr_matrix[x][y] else white_stone, (cx, cy), black_stone)
 
     board_image.save("go_qrcode.png", format="PNG")
@@ -115,8 +98,8 @@ data_string = "warwicksu.com/societies-sports/societies/gosociety/"
 create_go_board_with_qr(
     data_string,
     grid_size=90,
-    stone_scaling=0.9,
-    border_size=100,
-    background="transparent", # "color", "texture", or "transparent"
+    stone_scaling=0.95,
+    border_size=200,
+    background="texture", # "color", "texture", or "transparent"
     bg_color=(0,0,0,0)
 )
